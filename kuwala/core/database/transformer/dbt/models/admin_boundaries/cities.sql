62a115dbc4b807c41d793e5ea997115d7232cd0d
Matti Grotheer <matti@kuwala.io>
2022-01-18 17:15:06 +0100

    Match GeoName city names with OSM admin boundaries

---
+++ kuwala/core/database/transformer/dbt/models/admin_boundaries/cities.sql
@@ -0,0 +1,9 @@
+SELECT city_name, geometry
+FROM {{ ref('city_candidates') }}
+INNER JOIN (
+    SELECT city_name AS city_name_best_match, MIN(min_levenshtein_distance) AS min_levenshtein_distance_best_match
+    FROM {{ ref('city_candidates') }}
+    GROUP BY city_name
+) AS best_city_candidates ON
+    city_candidates.city_name = best_city_candidates.city_name_best_match AND
+    city_candidates.min_levenshtein_distance = best_city_candidates.min_levenshtein_distance_best_match
